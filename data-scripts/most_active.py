7b7924c921f9748849eef7e9494583ddfc522a84
rando_mane <hogand@sou.edu>
2021-04-04 15:08:24 -0700

    fix comments

--- data-scripts/most_active.py
+++ data-scripts/most_active.py
@@ -12,7 +12,7 @@ conn = psycopg2.connect(user=PG_USER,
                  )
 
 cursor = conn.cursor()
-# cursor.execute('delete from most_active')
+cursor.execute('delete from most_active')
 
 data = pd.read_html('https://www.marketwatch.com/tools/screener/premarket?mod=side_nav')
 most_active = data[2]
@@ -21,28 +21,7 @@ for ticker in most_active.values:
     
     cursor.execute('insert into most_active (ticker, company, price, change, volume) \
                     values (%s, %s, %s, %s, %s)',
-                    (gainer[0].split()[0], gainer[1], gainer[2], gainer[5], gainer[3]))
-
-
-
-
-
-
-# data = pd.read_html('https://www.marketwatch.com/tools/screener/premarket?mod=side_nav')
-# gainers = data[0]
-# losers = data[1]
-
-# for gainer in gainers.values:
-    
-#     cursor.execute('insert into premarket_gainers (ticker, company, price, change, volume) \
-#                     values (%s, %s, %s, %s, %s)',
-#                     (gainer[0].split()[0], gainer[1], gainer[2], gainer[5], gainer[3]))
-        
-# for loser in losers.values:
-    
-#     cursor.execute('insert into premarket_losers (ticker, company, price, change, volume) \
-#                     values (%s, %s, %s, %s, %s)',
-#                     (loser[0].split()[0], loser[1], loser[2], loser[5], loser[3]))
+                    (ticker[0].split()[0], ticker[1], ticker[2], ticker[5], ticker[3]))
 
 
 # *****************************************************************************
