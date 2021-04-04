ed224ff0c79f5ab197eefe1b788f046193909cf8
rando_mane <hogand@sou.edu>
2021-04-04 14:16:17 -0700

    insider_trading accumulates data

--- data-scripts/insider_trading.py
+++ data-scripts/insider_trading.py
@@ -14,13 +14,35 @@ conn = psycopg2.connect(user=PG_USER,
                  )
 
 cursor = conn.cursor()
-cursor.execute('delete from insider_trading')
+cursor.execute('select * from insider_trading')
+insider_trading = cursor.fetchall()
 
-insider_trading = pd.read_html('http://openinsider.com/insider-purchases-25k')[11]
-rm_columns = ['X', 'Trade Type', '1d', '1w', '1m', '6m'] 
-insider_trading = insider_trading[[column for column in insider_trading.columns if column not in rm_columns]]
+if len(insider_trading) > 0:
+    latest_in_db = insider_trading[0]
 
-for row in insider_trading.values:
+current_it = pd.read_html('http://openinsider.com/insider-purchases-25k')[11]
+rm_columns = ['X', '1d', '1w', '1m', '6m']
+current_it = current_it[[column for column in current_it.columns if column not in rm_columns]]
+
+for row in current_it.values:
+    if len(insider_trading) == 0:
+        pass
+    
+    elif (latest_in_db[0].strftime('%Y-%m-%d') == row[0][:10] and
+        latest_in_db[1].strftime('%Y-%m-%d') == row[1] and
+        latest_in_db[2] == row[2] and
+        latest_in_db[3] == row[3] and
+        latest_in_db[4] == row[4] and
+        latest_in_db[5] == row[5] and
+        float(latest_in_db[6]) == float(row[7][1:]) and
+        float(latest_in_db[7]) == float(row[8][1:].replace(',', '')) and
+        float(latest_in_db[8]) == float(row[9]) and
+        latest_in_db[9] == row[10] and
+        float(latest_in_db[10]) == float(row[11][1:].replace(',', '').replace('$', ''))):
+        
+        break
+    
+    
     cursor.execute('insert into insider_trading (filing_date, trade_date, ticker, company, insider, title, price, qty, owned, change, value) \
                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
                    (row[0][:10], row[1], row[2], row[3], row[4], row[5],
