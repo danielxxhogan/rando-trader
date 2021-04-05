189b3aaeee9d9a9c5f612e23c92504c8a95a01e5
rando_mane <hogand@sou.edu>
2021-04-04 23:05:52 -0700

    delete comments

--- data-scripts/insider_trading.py
+++ data-scripts/insider_trading.py
@@ -4,8 +4,6 @@ import datetime as dt
 
 from config import *
 
-# one_week_ago = (dt.datetime.today() - dt.timedelta(days=4))
-
 conn = psycopg2.connect(user=PG_USER,
                  password=PG_PASSWORD,
                  host=PG_HOST,
@@ -24,6 +22,7 @@ if len(insider_trading) > 0:
 current_it = pd.read_html('http://openinsider.com/insider-purchases-25k')[11]
 rm_columns = ['X', '1d', '1w', '1m', '6m']
 current_it = current_it[[column for column in current_it.columns if column not in rm_columns]]
+
 
 # USING RECURSION FOR THE FIRST TIME IN A PRACTICAL WAY
 # *****************************************************************************
@@ -52,52 +51,18 @@ def add_new(x=0):
     else:
         add_new(x+1)
         
-        cursor.execute('insert into insider_trading (filing_date, trade_date, ticker, company, insider, title, price, qty, owned, change, value) \
-               values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-               (row[0][:10], row[1], row[2], row[3], row[4], row[5],
-                float(row[7][1:].replace(',', '')),
-                float(row[8].replace(',', '')),
-                row[9], row[10],
-                float(row[11].replace(',', '').replace('$', ''))
-                ))
+        cursor.execute('insert into insider_trading (filing_date, trade_date, \
+                        ticker, company, insider, title, price, qty, owned, change, value) \
+                        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                        (row[0][:10], row[1], row[2], row[3], row[4], row[5],
+                         float(row[7][1:].replace(',', '')),
+                         float(row[8].replace(',', '')),
+                         row[9], row[10],
+                         float(row[11].replace(',', '').replace('$', ''))
+                         ))
             
 add_new()
 # *****************************************************************************
-conn.commit()
-
-cursor.close()
-conn.close()
-
-
-# for row in current_it.values:
-#     if len(insider_trading) == 0:
-#         pass
-    
-#     elif (latest_in_db[0].strftime('%Y-%m-%d') == row[0][:10] and
-#         latest_in_db[1].strftime('%Y-%m-%d') == row[1] and
-#         latest_in_db[2] == row[2] and
-#         latest_in_db[3] == row[3] and
-#         latest_in_db[4] == row[4] and
-#         latest_in_db[5] == row[5] and
-#         float(latest_in_db[6]) == float(row[7][1:]) and
-#         float(latest_in_db[7]) == float(row[8][1:].replace(',', '')) and
-#         float(latest_in_db[8]) == float(row[9]) and
-#         latest_in_db[9] == row[10] and
-#         float(latest_in_db[10]) == float(row[11][1:].replace(',', '').replace('$', ''))):
-        
-#         break
-    
-    
-#     cursor.execute('insert into insider_trading (filing_date, trade_date, ticker, company, insider, title, price, qty, owned, change, value) \
-#                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-#                    (row[0][:10], row[1], row[2], row[3], row[4], row[5],
-#                     float(row[7][1:].replace(',', '')),
-#                     float(row[8].replace(',', '')),
-#                     row[9], row[10],
-#                     float(row[11].replace(',', '').replace('$', ''))
-#                     ))
-
-
 conn.commit()
 
 cursor.close()
