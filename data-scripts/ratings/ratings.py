adb1010798ea6a92d571b58a6f297f23c05fc627
rando_mane <hogand@sou.edu>
2021-04-12 23:29:37 -0700

    get downgrades

--- data-scripts/ratings/ratings.py
+++ data-scripts/ratings/ratings.py
@@ -34,6 +34,24 @@ for line in upgrades:
         row_string = line    
     else:
         row_string += (f' {line}')
+
+
+browser = webdriver.Chrome()
+browser.get('https://www.benzinga.com/analyst-ratings/downgrades')
+downgrades = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="analyst-calendar"]/div/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div/div'))).text.split('\n')
+browser.quit()
+
+row_string = ''
+for line in downgrades:
+    
+    if re.search('[0-9]{2}/[0-9]{2}/[0-9]{4}', line):
+        if len(row_string.split()) > 1:
+            ticker = row_string.split()[1]
+            print(ticker)
+            
+        row_string = line    
+    else:
+        row_string += (f' {line}')
         
     
         
