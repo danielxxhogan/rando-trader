5f3e3f10501164cba060319d534dca3be3c18f4d
rando_mane <hogand@sou.edu>
2021-05-20 18:04:13 -0700

    stuff

--- data-api/index.py
+++ data-api/index.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import psycopg2
+
+from config import *
+
 
 app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app.run(port=3004)
+@app.route('/test', methods=['GET'])
+def test():
+    conn = psycopg2.connect(user=PG_USER,
+                 password=PG_PASSWORD,
+                 host=PG_HOST,
+                 port=PG_PORT,
+                 dbname=PG_DATABASE,
+                 )
+
+    cursor = conn.cursor()
+    
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    
+    return 'test'
+
+
+if __name__ == '__main__':
+    app.run(port=3003)
