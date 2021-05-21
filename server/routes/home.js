2a137cd60d2696d01cefdefaf68bcf2c42ffa5ee
rando_mane <hogand@sou.edu>
2021-05-21 14:31:28 -0700

    server users request package to talk to data-api

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
 const fetch = require('node-fetch');
+const request = require('request');
 
 router.get('/premarket-gainers', async (req, res) => {
   try {
     // res.json('hi');
     console.log('here');
-    const response = await fetch('https://localhost:3002/premarket-gainers');
-    console.log('here');
-    console.log(response);
-    res.json(response);
+    // const response = await fetch('https://localhost:3002/premarket-gainers');
+    const response = request('http://localhost:3002/premarket-gainers', (err, reqres) => {
+      if (err) return console.error(err.message);
+
+      console.log(reqres.body);
+      res.json(reqres.body)
+
+    });
+
+
+
+    // console.log('here');
+    // console.log(response);
+    // res.json(response);
 
     
   } catch (err) {
