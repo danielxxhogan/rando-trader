2f94e8bf028e81286dfb7dc4c0a9c7523b2267a1
rando_mane <hogand@sou.edu>
2021-05-22 18:51:29 -0700

    make make functions for all quiver tables

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -85,7 +85,7 @@ const Home = () => {
 
 
 
-  const makeTableRow = (row) => {
+  const makePremarket = (row) => {
     return <>
       <tr>
         <td>{row.ticker}</td>
@@ -93,6 +93,98 @@ const Home = () => {
         <td>{row.price}</td>
         <td>{row.change}</td>
         <td>{row.volume}</td>
+      </tr>
+    </>
+  }
+
+  const makeInsider = (row) => {
+    return <>
+      <tr>
+        <td>{row.filing_date}</td>
+        <td>{row.trade_date}</td>
+        <td>{row.company}</td>
+        <td>{row.insider}</td>
+        <td>{row.title}</td>
+        <td>{row.price}</td>
+        <td>{row.qty}</td>
+        <td>{row.owned}</td>
+        <td>{row.change}</td>
+        <td>{row.value}</td>
+      </tr>
+    </>
+  }
+
+  const makeShortInterest = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.company}</td>
+        <td>{row.price}</td>
+        <td>{row.short_interest}</td>
+        <td>{row.float}</td>
+        <td>{row.float_shorted}</td>
+      </tr>
+    </>
+  }
+
+  const makeContracts = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.date}</td>
+        <td>{row.agency}</td>
+        <td>{row.amount}</td>
+        <td>{row.description}</td>
+      </tr>
+    </>
+  }
+
+  const makeLobbying = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.date}</td>
+        <td>{row.amount}</td>
+        <td>{row.client}</td>
+        <td>{row.issue}</td>
+      </tr>
+    </>
+  }
+
+  const makeCongress = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.report_date}</td>
+        <td>{row.transaction_date}</td>
+        <td>{row.amount}</td>
+        <td>{row.transaction}</td>
+        <td>{row.representative}</td>
+        <td>{row.house}</td>
+      </tr>
+    </>
+  }
+
+  const makeSenate = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.date}</td>
+        <td>{row.amount}</td>
+        <td>{row.transaction}</td>
+        <td>{row.senator}</td>
+      </tr>
+    </>
+  }
+
+  const makeHouse = (row) => {
+    return <>
+      <tr>
+        <td>{row.ticker}</td>
+        <td>{row.date}</td>
+        <td>{row.amount}</td>
+        <td>{row.transaction}</td>
+        <td>{row.representative}</td>
       </tr>
     </>
   }
@@ -105,61 +197,61 @@ const Home = () => {
 
     <h2>Premarket Gainers</h2>
     <table>
-      {premarketGainers && premarketGainers.map(row => { return makeTableRow(row) })}
+      {premarketGainers && premarketGainers.map(row => { return makePremarket(row) })}
     </table>
 
     <hr />
     <h2>Premarket Losers</h2>
     <table>
-    {premarketLosers && premarketLosers.map(row => { return makeTableRow(row) })}
+    {premarketLosers && premarketLosers.map(row => { return makePremarket(row) })}
     </table>
 
     <hr />
     <h2>Most Active</h2>
     <table>
-    {mostActive && mostActive.map(row => { return makeTableRow(row) })}
+    {mostActive && mostActive.map(row => { return makePremarket(row) })}
     </table>
 
     <hr />
     <h2>Insider Trading</h2>
     <table>
-    {insiderTrading && insiderTrading.map(row => { return makeTableRow(row) })}
+    {insiderTrading && insiderTrading.map(row => { return makeInsider(row) })}
     </table>
 
     <hr />
     <h2>Short Interest</h2>
     <table>
-    {shortInterest && shortInterest.map(row => { return makeTableRow(row) })}
+    {shortInterest && shortInterest.map(row => { return makeShortInterest(row) })}
     </table>
 
     <hr />
     <h2>Government Contracts</h2>
     <table>
-    {contracts && contracts.map(row => { return makeTableRow(row) })}
+    {contracts && contracts.map(row => { return makeContracts(row) })}
     </table>
 
     <hr />
     <h2>Corporate Lobbying</h2>
     <table>
-    {lobbying && lobbying.map(row => { return makeTableRow(row) })}
+    {lobbying && lobbying.map(row => { return makeLobbying(row) })}
     </table>
 
     <hr />
     <h2>Congress Trades</h2>
     <table>
-    {congress && congress.map(row => { return makeTableRow(row) })}
+    {congress && congress.map(row => { return makeCongress(row) })}
     </table>
 
     <hr />
     <h2>Senate Trades</h2>
     <table>
-    {senate && senate.map(row => { return makeTableRow(row) })}
+    {senate && senate.map(row => { return makeSenate(row) })}
     </table>
 
     <hr />
     <h2>House Trades</h2>
     <table>
-    {house && house.map(row => { return makeTableRow(row) })}
+    {house && house.map(row => { return makeHouse(row) })}
     </table>
 
   </>
