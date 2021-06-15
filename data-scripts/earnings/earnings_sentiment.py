07a57918bc5683509642a22ef823374f8f3cbea4
rando_mane <hogand@sou.edu>
2021-06-15 11:25:48 -0700

    remove old code and prints

--- data-scripts/earnings/earnings_sentiment.py
+++ data-scripts/earnings/earnings_sentiment.py
@@ -46,21 +46,11 @@ for i in range(len(earnings)):
         
         s = Sentiment(ticker)
         news = s.get_news()
-        print(news)
         stocktwits = s.get_stocktwits()
-        print(stocktwits)
         press_releases = s.get_press_releases()
-        print(press_releases)
         analyst_ratings = s.get_analyst_ratings()
-        print(analyst_ratings)
         insider_trading = s.get_insider_trading()
-        print(insider_trading)
         quiver_data = s.get_quiver_data()
-        print(quiver_data)
-        # break
-        
-        # ticker_sentiment = sentiment.calculate_sentiment(f'{ticker}')
-        # print(ticker_sentiment)
         
         cursor.execute('insert into earnings_sentiment (ticker, articles, sentiment, \
             today_sentiment, messages, today_sentiment_st, sentiment_st, \
@@ -77,17 +67,6 @@ for i in range(len(earnings)):
              quiver_data['senate_buys'], quiver_data['senate_sales'],
              quiver_data['house_buys'], quiver_data['house_sales'],
              insider_trading, analyst_ratings['upgrades'], analyst_ratings['downgrades']))
-    
-    
-            # (ticker, ticker_sentiment['articles'], ticker_sentiment['sentiment'],
-            # ticker_sentiment['today_sentiment'], ticker_sentiment['messages'],
-            # ticker_sentiment['today_sentiment_st'], ticker_sentiment['sentiment_st'],
-            # ticker_sentiment['press_releases'], ticker_sentiment['contracts'],
-            # ticker_sentiment['lobbying'], ticker_sentiment['congress_buys'],
-            # ticker_sentiment['congress_sales'], ticker_sentiment['senate_buys'],
-            # ticker_sentiment['senate_sales'], ticker_sentiment['house_buys'],
-            # ticker_sentiment['house_sales'], ticker_sentiment['insider_trades'],
-            # ticker_sentiment['upgrades'], ticker_sentiment['downgrades']))
         
         conn.commit()
 
