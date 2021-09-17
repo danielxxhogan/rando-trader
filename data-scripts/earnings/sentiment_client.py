f97cdb2da32172b916f64827edb99b170a3b4a6f
rando-mane <dhogan382444@yahoo.com>
2021-09-17 12:32:02 -0700

    testing new credentials

--- data-scripts/earnings/sentiment_client.py
+++ data-scripts/earnings/sentiment_client.py
@@ -9,17 +9,17 @@ print(dir(s))
 results = s.get_stocktwits()
 print('\n\n LAST LINE:', results)
 
-results = s.get_insider_trading()
-print('\n\n LAST LINE:', results)
+# results = s.get_insider_trading()
+# print('\n\n LAST LINE:', results)
 
-results = s.get_news()
-print('\n\n LAST LINE:', results)
+# results = s.get_news()
+# print('\n\n LAST LINE:', results)
 
-results = s.get_press_releases()
-print('\n\n LAST LINE:', results)
+# results = s.get_press_releases()
+# print('\n\n LAST LINE:', results)
 
-results = s.get_quiver_data()
-print('\n\n LAST LINE:', results)
+# results = s.get_quiver_data()
+# print('\n\n LAST LINE:', results)
 
-results = s.get_analyst_ratings()
-print('\n\n LAST LINE:', results)
+# results = s.get_analyst_ratings()
+# print('\n\n LAST LINE:', results)
